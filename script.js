6606545c3c36ef23e00891af27bc304ed1ca3f01
rambroise <55062809+rambroise@users.noreply.github.com>
2019-12-10 21:47:19 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,101 @@
+function reveal1() {
+    document.getElementById("Answer1").style.display= 'block';
+}
+
+function reveal2() {
+    document.getElementById("Answer2").style.display='block';
+}
+
+function reveal3() {
+    document.getElementById("Answer3").style.display='block';
+}
+
+function reveal4() {
+    document.getElementById("Answer4").style.display='block';
+}
+
+function reveal5() {
+    document.getElementById("Answer5").style.display='block';
+}
+
+function reveal6() {
+    document.getElementById("Answer6").style.display='block';
+}
+
+function reveal7() {
+    document.getElementById("Answer7").style.display='block';
+}
+
+function reveal8() {
+    document.getElementById("Answer8").style.display='block';
+}
+
+function reveal9() {
+    document.getElementById("Answer9").style.display='block';
+}
+
+function reveal10() {
+    document.getElementById("Answer10").style.display='block';
+}
+
+
+
+
+function next1() {
+    document.getElementById("takeaway1").style.display= 'block';
+}
+
+function next2() {
+    document.getElementById("case2").style.display= 'block';
+    document.getElementById("case1").style.display='none';
+}
+
+function next3()
+{
+    document.getElementById("takeaway2").style.display='block';
+    document.getElementById("next3").style.display='block';
+}
+
+function next4()
+{
+    document.getElementById("case3").style.display= 'block';
+    document.getElementById("case2").style.display='none';
+}
+
+function next5()
+{
+    document.getElementById("takeaway3").style.display='block';
+}
+
+function next6()
+{
+    document.getElementById("takeaway4").style.display='block';
+}
+
+function next7()
+{
+    document.getElementById("case5").style.display='block';
+    document.getElementById("case4").style.display='none';
+}
+
+function next8()
+{
+    document.getElementById("takeaway5").style.display='block';
+}
+
+function next9()
+{
+    document.getElementById("takeaway6").style.display='block';
+}
+
+function next10()
+{
+    document.getElementById("case6").style.display='block';
+    document.getElementById("case5").style.display='none';
+}
+
+function next11()
+{
+    document.getElementById("done").style.display='block';
+    document.getElementById("conclusion").style.display='none';
+}
